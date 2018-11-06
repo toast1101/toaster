b5f362ea42079144191415524023a3a154212202
toast1101 <44804456+toast1101@users.noreply.github.com>
2018-11-06 20:24:30 +0800

    我剛剛加了if條件式

--- main_toaster/main_toaster.ino
+++ main_toaster/main_toaster.ino
@@ -8,8 +8,16 @@ int sensorValue;
 int valueGate = 100;
 int sensorState;
 void loop() {
-  if(sensorValue>valueGate){
-    if(sensorValue<)
+  if(sensorValue<100){
+    
+  }
+  else if(sensorValue<200){
+    
+  }
+  else if(sensorValue<300){
+    
+  }
+  else if(sensorValue<400){
     
   }
 
