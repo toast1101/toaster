3a13e12623cdfcf65746b45df6ce13f8101b69c2
toast1101 <44804456+toast1101@users.noreply.github.com>
2018-11-06 20:30:46 +0800

    add valueState

--- main_toaster/main_toaster.ino
+++ main_toaster/main_toaster.ino
@@ -4,21 +4,26 @@ void setup() {
   
 
 }
+int valueState;
 int sensorValue;
 int valueGate = 100;
 int sensorState;
 void loop() {
   if(sensorValue<100){
-    
+    valueState = 1;
   }
   else if(sensorValue<200){
-    
+    valueState = 2;
   }
   else if(sensorValue<300){
-    
+    valueState = 3;
   }
   else if(sensorValue<400){
-    
+    valueState = 4;
   }
+  delay(1000);
+  Serial.println("=====wryyyyyyy=====");
+  Serial.println("valueState: " + valueState);
+  
 
 }
