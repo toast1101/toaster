75b884ea0dabfcf4e4566ae7628df4252e80e4f3
toast1101 <44804456+toast1101@users.noreply.github.com>
2018-11-06 20:19:57 +0800

    move file to folder and add new file

---
+++ main_toaster/main_toaster.ino
@@ -0,0 +1,16 @@
+void setup() {
+  Serial.begin(9600);
+  Serial.print("===========Start!===========");
+  
+
+}
+int sensorValue;
+int valueGate = 100;
+int sensorState;
+void loop() {
+  if(sensorValue>valueGate){
+    if(sensorValue<)
+    
+  }
+
+}
